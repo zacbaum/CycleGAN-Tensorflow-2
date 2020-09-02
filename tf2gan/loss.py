5d547ec0e2e56ccb103758502bcfcab4b693c1b6
Zachary Baum <zac_baum@hotmail.com>
2020-09-02 13:24:37 +0100

    Remove grad penalties from loss

--- tf2gan/loss.py
+++ tf2gan/loss.py
@@ -81,35 +81,3 @@ def get_adversarial_losses_fn(mode):
         return get_lsgan_losses_fn()
     elif mode == 'wgan':
         return get_wgan_losses_fn()
-
-
-def gradient_penalty(f, real, fake, mode):
-    def _gradient_penalty(f, real, fake=None):
-        def _interpolate(a, b=None):
-            if b is None:   # interpolation in DRAGAN
-                beta = tf.random.uniform(shape=tf.shape(a), minval=0., maxval=1.)
-                b = a + 0.5 * tf.math.reduce_std(a) * beta
-            shape = [tf.shape(a)[0]] + [1] * (a.shape.ndims - 1)
-            alpha = tf.random.uniform(shape=shape, minval=0., maxval=1.)
-            inter = a + alpha * (b - a)
-            inter.set_shape(a.shape)
-            return inter
-
-        x = _interpolate(real, fake)
-        with tf.GradientTape() as t:
-            t.watch(x)
-            pred = f(x)
-        grad = t.gradient(pred, x)
-        norm = tf.norm(tf.reshape(grad, [tf.shape(grad)[0], -1]), axis=1)
-        gp = tf.reduce_mean((norm - 1.)**2)
-
-        return gp
-
-    if mode == 'none':
-        gp = tf.constant(0, dtype=real.dtype)
-    elif mode == 'dragan':
-        gp = _gradient_penalty(f, real)
-    elif mode == 'wgan-gp':
-        gp = _gradient_penalty(f, real, fake)
-
-    return gp
